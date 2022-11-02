100e4710ba52531f3447f71f5befaedbae6f53c4
Rodolfo Scarp <rodolfoscarp@gmail.com>
2022-11-01 22:11:27 -0300

    Adicionado tests da api cliente e vendedor

--- cliente/tests.py
+++ cliente/tests.py
@@ -2,20 +2,133 @@ from django.urls import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status
 from .models import Cliente
+from django.forms.models import model_to_dict
 
 
-class ClienteApiTests(APITestCase):
+def make_cliente(**kwargs):
+
+    return {
+        'nome': kwargs.get('nome', 'Cliente1'),
+        'email': kwargs.get('email', 'cliente1@cliente.com.br'),
+        'telefone': kwargs.get('telefone', '(24) 99999-9999')
+    }
+
+
+class CreateClienteTests(APITestCase):
+
+    def setUp(self) -> None:
+        self.url = reverse('cliente-list')
+
     def test_deve_cadastra_novo_cliente(self):
-        url = reverse('cliente-list')
-        data = {
-            'nome': 'Cliente1',
-            'email': 'cliente@cliente.com.br',
-            'telefone': '(24) 99999-9999'
-        }
+        cliente_data = make_cliente()
 
-        res = self.client.post(url, data, format='json')
+        res = self.client.post(self.url, cliente_data, format='json')
 
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         self.assertEqual('Cliente1', Cliente.objects.get().nome)
-        self.assertEqual('cliente@cliente.com.br', Cliente.objects.get().email)
+        self.assertEqual('cliente1@cliente.com.br',
+                         Cliente.objects.get().email)
         self.assertEqual('(24) 99999-9999', Cliente.objects.get().telefone)
+
+    def test_nao_deve_cadastrar_novo_cliente_sem_nome(self):
+        cliente_data = make_cliente(nome=None)
+
+        res = self.client.post(self.url, cliente_data, format='json')
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_nao_deve_cadastrar_novo_cliente_sem_telefone(self):
+        cliente_data = make_cliente(telefone=None)
+
+        res = self.client.post(self.url, cliente_data, format='json')
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_nao_deve_cadastrar_novo_cliente_sem_email(self):
+        cliente_data = make_cliente(email=None)
+
+        res = self.client.post(self.url, cliente_data, format='json')
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_nao_deve_cadastrar_novo_cliente_com_email_invalido(self):
+        cliente_data = make_cliente(email='cliente.cliente.com.br')
+
+        res = self.client.post(self.url, cliente_data, format='json')
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_nao_deve_cadastrar_novo_cliente_com_telefone_invalido(self):
+        cliente_data = make_cliente(telefone='55885588')
+
+        res = self.client.post(self.url, cliente_data, format='json')
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class ListClienteTests(APITestCase):
+    def setUp(self) -> None:
+        self.url = reverse('cliente-list')
+        self.cliente1 = dict(
+            nome='Cliente1', email='cliente1@cliente.com.br',
+            telefone='(21) 99999-9999'
+        )
+
+        self.cliente2 = dict(
+            nome='Cliente2', email='cliente2@cliente.com.br',
+            telefone='(22) 99999-9999'
+        )
+
+        self.cliente3 = dict(
+            nome='Cliente3', email='cliente3@cliente.com.br',
+            telefone='(23) 99999-9999'
+        )
+
+        Cliente.objects.create(**self.cliente1)
+        Cliente.objects.create(**self.cliente2)
+        Cliente.objects.create(**self.cliente3)
+
+    def test_deve_listar_todos_os_clientes_cadastrados(self):
+        clientes = self.client.get(self.url)
+
+        clientes.json()
+
+        self.assertEqual(clientes.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(clientes.json()), 3)
+
+    def test_deve_listar_um_cliente(self):
+        cliente1 = Cliente.objects.get(id=1)
+
+        res = self.client.get(f'{self.url}1/')
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.json(), model_to_dict(cliente1))
+
+
+class UpdateClienteTests(APITestCase):
+    def setUp(self) -> None:
+        self.url = reverse('cliente-list')
+        self.cliente1 = make_cliente()
+
+        Cliente.objects.create(**self.cliente1)
+
+    def test_deve_atualizar_todos_os_campos_do_cliente(self):
+
+        cliente_data = make_cliente(
+            nome='Cliente2', email='cliente2@cliente.com.br',
+            telefone='(22) 99999-9999'
+        )
+
+        res = self.client.put(self.url + '1/', cliente_data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+        self.assertEqual('Cliente2', Cliente.objects.get().nome)
+        self.assertEqual('cliente2@cliente.com.br',
+                         Cliente.objects.get().email)
+        self.assertEqual('(22) 99999-9999', Cliente.objects.get().telefone)
+
+    def test_deve_atualizar_um_campo_do_cliente(self):
+
+        res = self.client.patch(self.url + '1/', {'nome': 'Cliente3'})
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual('Cliente3', Cliente.objects.get().nome)
--- vendedor/tests.py
+++ vendedor/tests.py
@@ -1,3 +1,134 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from .models import Vendedor
+from django.forms.models import model_to_dict
 
-# Create your tests here.
+
+def make_vendedor(**kwargs):
+
+    return {
+        'nome': kwargs.get('nome', 'Vendedor1'),
+        'email': kwargs.get('email', 'vendedor1@vendedor.com.br'),
+        'telefone': kwargs.get('telefone', '(24) 99999-9999')
+    }
+
+
+class CreateVendedorTests(APITestCase):
+
+    def setUp(self) -> None:
+        self.url = reverse('vendedor-list')
+
+    def test_deve_cadastra_novo_vendedor(self):
+        vendedor_data = make_vendedor()
+
+        res = self.client.post(self.url, vendedor_data, format='json')
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        self.assertEqual('Vendedor1', Vendedor.objects.get().nome)
+        self.assertEqual('vendedor1@vendedor.com.br',
+                         Vendedor.objects.get().email)
+        self.assertEqual('(24) 99999-9999', Vendedor.objects.get().telefone)
+
+    def test_nao_deve_cadastrar_novo_vendedor_sem_nome(self):
+        vendedor_data = make_vendedor(nome=None)
+
+        res = self.client.post(self.url, vendedor_data, format='json')
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_nao_deve_cadastrar_novo_vendedor_sem_telefone(self):
+        vendedor_data = make_vendedor(telefone=None)
+
+        res = self.client.post(self.url, vendedor_data, format='json')
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_nao_deve_cadastrar_novo_vendedor_sem_email(self):
+        vendedor_data = make_vendedor(email=None)
+
+        res = self.client.post(self.url, vendedor_data, format='json')
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_nao_deve_cadastrar_novo_vendedor_com_email_invalido(self):
+        vendedor_data = make_vendedor(email='.cliente.com.br')
+
+        res = self.client.post(self.url, vendedor_data, format='json')
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_nao_deve_cadastrar_novo_vendedor_com_telefone_invalido(self):
+        vendedor_data = make_vendedor(telefone='55885588')
+
+        res = self.client.post(self.url, vendedor_data, format='json')
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class ListVendedorTests(APITestCase):
+    def setUp(self) -> None:
+        self.url = reverse('vendedor-list')
+        self.vendedor1 = dict(
+            nome='Vendedor1', email='vendedor1@vendedor.com.br',
+            telefone='(21) 99999-9999'
+        )
+
+        self.vendedor2 = dict(
+            nome='Vendedor2', email='2@vendedor.com.br',
+            telefone='(22) 99999-9999'
+        )
+
+        self.vendedor3 = dict(
+            nome='Vendedor3', email='3@vendedor.com.br',
+            telefone='(23) 99999-9999'
+        )
+
+        Vendedor.objects.create(**self.vendedor1)
+        Vendedor.objects.create(**self.vendedor2)
+        Vendedor.objects.create(**self.vendedor3)
+
+    def test_deve_listar_todos_os_vendedores_cadastrados(self):
+        vendedores = self.client.get(self.url)
+
+        vendedores.json()
+
+        self.assertEqual(vendedores.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(vendedores.json()), 3)
+
+    def test_deve_listar_um_vendedor(self):
+        vendedor1 = Vendedor.objects.get(id=1)
+
+        res = self.client.get(f'{self.url}1/')
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.json(), model_to_dict(vendedor1))
+
+
+class UpdateVendedorTests(APITestCase):
+    def setUp(self) -> None:
+        self.url = reverse('vendedor-list')
+        self.vendedor1 = make_vendedor()
+
+        Vendedor.objects.create(**self.vendedor1)
+
+    def test_deve_atualizar_todos_os_campos_do_vendedor(self):
+
+        vendedor_data = make_vendedor(
+            nome='Vendedor2', email='vendedor2@vendedor.com.br',
+            telefone='(22) 99999-9999'
+        )
+
+        res = self.client.put(self.url + '1/', vendedor_data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+        self.assertEqual('Vendedor2', Vendedor.objects.get().nome)
+        self.assertEqual('vendedor2@vendedor.com.br',
+                         Vendedor.objects.get().email)
+        self.assertEqual('(22) 99999-9999', Vendedor.objects.get().telefone)
+
+    def test_deve_atualizar_um_campo_do_vendedor(self):
+
+        res = self.client.patch(self.url + '1/', {'nome': 'vendedor3'})
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual('vendedor3', Vendedor.objects.get().nome)
